6035c78905f831bbeecfcc590cdd94f3624bcc10
Isha Sharma <isha.sharma@msn.com>
2021-02-12 19:22:01 -0800

    added LargestCommonPrefix

---
+++ LeetCode/LargestCommonPrefix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs == []:
+            return ""
+    
+        common = ""
+        minLength = min(strs,key = len)
+        if minLength == 0:
+            return common
+        flag = True
+        for i in range (len(minLength)):
+            prev = minLength[i]
+            
+            for item in range(len(strs)):
+                if(strs[item][i] != prev):
+                    flag = False
+                    
+            if flag == True:
+                common = common + minLength[i]
+                
+        return common
